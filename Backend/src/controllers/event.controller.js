f0ac571b5251949e626c01d7f1d763bbb7af4150
unknown <james.r@kalvium.community>
2025-05-14 22:01:56 +0530

    read and write performed

--- Backend/src/controllers/event.controller.js
+++ Backend/src/controllers/event.controller.js
@@ -5,7 +5,7 @@ const getAllEvents = async (req,res) => {
         const events = await Event.find();
         res.status(200).json(events);
     }catch(error) {
-        res.status(500).json({message: 'Server Error',error});
+        res.status(500).json({message: 'Internal Server Error',error});
     }
 }
 
@@ -20,8 +20,8 @@ const createEvent = async (req,res) =>{
         createdBy,
     } = req.body
 
-if(!title || !date){
-    return res.status(400).json({error:'Title and Date are required'})
+if(!title ||!date){
+    return res.status(400).json({error:'Title and Date are required.'})
 }
 
 try {

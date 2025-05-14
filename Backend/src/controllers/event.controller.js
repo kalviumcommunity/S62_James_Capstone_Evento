a00b8af37df345169f77160227d9eaf3fe72f1b8
unknown <james.r@kalvium.community>
2025-05-14 22:28:25 +0530

    implemented relationship

--- Backend/src/controllers/event.controller.js
+++ Backend/src/controllers/event.controller.js
@@ -2,7 +2,7 @@ const Event = require('../models/event.model');
 
 const getAllEvents = async (req,res) => {
     try {
-        const events = await Event.find();
+        const events = await Event.find().populate('createdBy', 'name email');
         res.status(200).json(events);
     }catch(error) {
         res.status(500).json({message: 'Internal Server Error',error});

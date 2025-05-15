a71dcdf9d684084686a5138ac478469145c3cd82
unknown <james.r@kalvium.community>
2025-05-15 15:53:10 +0530

    jwt_implemented

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken')
 
+
 const verifyToken = (req,res, next)=>{
-    const token = req.header.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(' ')[1];
 
     if(!token) return res.this.status(403).json({message:'Token expired'})
 
